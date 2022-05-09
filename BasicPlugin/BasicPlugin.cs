f8dca5459f4eebabab9324d21417d6376f027267
Dogel <oska.majo@gmail.com>
2022-05-09 07:41:20 +0100

    Add files via upload

---
+++ BasicPlugin/BasicPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Exiled.API.Features;
+using Player = Exiled.Events.Handlers.Player;
+
+public class BasicPlugin : Plugin<Config> {
+
+	public EventHandlers EventHandler;
+
+	public override string Name => "Basic Plugin";
+	public override string Author => "Dogel";
+	public override void OnEnabled() {
+		try {
+			EventHandler = new EventHandlers();
+			Player.Joined += EventHandler.PlayerJoined;
+		}
+		catch (Exception arg) {
+			Log.Error($"Loading error: {arg}");
+		}
+	}
+	public override void OnDisabled() {
+		Player.Joined -= EventHandler.PlayerJoined;
+		EventHandler = null;
+	}
+}
---
+++ BasicPlugin/EventHandlers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+public class EventHandlers : Plugin<Config> {
+	private Random rnd = new Random();
+	public void PlayerJoined(JoinedEventArgs ev) {
+		if(ev.Player.IsConnected) {
+			ev.Player.Broadcast(5, $"<color=yellow>Witaj</color> w Watkanie {ev.Player.Nickname}!/nYour current ping is {ev.Player.Ping}ms");
+			ev.Player.DisplayNickname = $"D-{rnd.Next(1, 7000)}";
+
+			if(ev.Player.DoNotTrack) {
+				ev.Player.Broadcast(10, Config.TrackMessage);
+			}
+		}
+	}
+}
