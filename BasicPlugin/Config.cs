8dde7ca632edc92b24a0a20d986da7a08fe34f58
Dogel <oska.majo@gmail.com>
2022-05-10 21:44:26 +0000

    Update Config.cs

--- BasicPlugin/Config.cs
+++ BasicPlugin/Config.cs
@@ -1,6 +1,6 @@
 ﻿using Exiled.API.Interfaces;
 
 public class Config : IConfig {
-	public bool IsEnabled { get; set; }
+	public bool IsEnabled { get; set; } = true;
 	public string TrackMessage { get; set; } = "<color=yellow>Aby cieszyć się z rozgrywki na najwyższym poziomie, zalecamy wyłączyć </color><color=lime>DNT</color>";
 }
