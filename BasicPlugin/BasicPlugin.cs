6534ec979819bb8d0fe149dc0916d827cbcea746
Dogel <oska.majo@gmail.com>
2022-05-10 21:04:15 +0000

    Add files via upload

--- BasicPlugin/BasicPlugin.cs
+++ BasicPlugin/BasicPlugin.cs
@@ -3,11 +3,12 @@ using Exiled.API.Features;
 using Player = Exiled.Events.Handlers.Player;
 
 public class BasicPlugin : Plugin<Config> {
-
 	public EventHandlers EventHandler;
 
 	public override string Name => "Basic Plugin";
 	public override string Author => "Dogel";
+	public override Version Version => new Version(1,0,1);
+
 	public override void OnEnabled() {
 		try {
 			EventHandler = new EventHandlers();
--- BasicPlugin/EventHandlers.cs
+++ BasicPlugin/EventHandlers.cs
@@ -5,11 +5,11 @@ using Exiled.Events.EventArgs;
 public class EventHandlers : Plugin<Config> {
 	private Random rnd = new Random();
 	public void PlayerJoined(JoinedEventArgs ev) {
-		if(ev.Player.IsConnected) {
-			ev.Player.Broadcast(5, $"<color=yellow>Witaj</color> w Watkanie {ev.Player.Nickname}!/nYour current ping is {ev.Player.Ping}ms");
-			ev.Player.DisplayNickname = $"D-{rnd.Next(1, 7000)}";
+		if(ev.Player.IsConnected == true) {
+			ev.Player.Broadcast(5, $"Witaj w <color=yellow>Watkanie</color> {ev.Player.Nickname}!");
+			ev.Player.DisplayNickname = $"Dumbass nr.{rnd.Next(1, 7000)}";
 
-			if(ev.Player.DoNotTrack) {
+			if(ev.Player.DoNotTrack == true) {
 				ev.Player.Broadcast(10, Config.TrackMessage);
 			}
 		}
