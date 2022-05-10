25be137e0c81c68ef244e60fb5506b0a2b579c95
Dogel <oska.majo@gmail.com>
2022-05-10 21:26:33 +0000

    Add files via upload

---
+++ BasicPlugin/Config.cs
@@ -0,0 +1,6 @@
+﻿using Exiled.API.Interfaces;
+
+public class Config : IConfig {
+	public bool IsEnabled { get; set; }
+	public string TrackMessage { get; set; } = "<color=yellow>Aby cieszyć się z rozgrywki na najwyższym poziomie, zalecamy wyłączyć </color><color=lime>DNT</color>";
+}
